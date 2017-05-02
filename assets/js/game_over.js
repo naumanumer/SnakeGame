9d86be756e103be4f175c241505092736d039e4b
Nauman Umer <u.nauman@outlook.com>
2017-05-02 15:56:08 +0500

    score bug fix

--- assets/js/game_over.js
+++ assets/js/game_over.js
@@ -2,11 +2,12 @@ var Game_Over = {
     create : function() {
         var menu = document.getElementsByClassName("menu")[0];
         var over = document.getElementsByClassName("over")[0];
+        var scopeSpan = document.getElementById("score");
 
 
         menu.style.display = "block";
         over.style.display = "block";
-
+        scopeSpan.innerText = score;
 
     }
 };
